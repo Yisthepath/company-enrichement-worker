341fede9a92a7da66ed42ead030e9f6fa7ce484b
Yisthepath <130940873+Yisthepath@users.noreply.github.com>
2026-05-04 21:12:28 -0400

    Extend HTML parser to extract headers and links

--- src/parser.py
+++ src/parser.py
@@ -11,6 +11,8 @@ def parse_html(html):
         - dict: a dicttionary containing the following data
             - "title" (str | None): the page's title
             - "description" (str | None): the page's description
+            - "headers" (list | None): the page's headers
+            - "links" (list | None)": the page's links
     """
 
     parsed_dict = {}
@@ -18,12 +20,18 @@ def parse_html(html):
     title_tag = soup.find("title")
     description_tag = soup.find("meta", attrs= {"name": "description"})
     description_og_tag = soup.find("meta", attrs= {"property": "og:description"})
+    header_tags = soup.find_all(["h1", "h2", "h3", "h4", "h5", "h6"])
+    a_tags = soup.find_all("a")
+
+    page_headers = []
+    page_links = []
 
     if title_tag != None:
         page_title = title_tag
         page_title = str(page_title.get_text(strip=True))
     else:
         page_title = None
+
     if description_tag!= None:
         page_description = description_tag
         page_description = str(page_description.get("content")).strip()
@@ -33,7 +41,32 @@ def parse_html(html):
     else:
         page_description = None
     
+    if list(header_tags) != []:
+        for tag in header_tags:
+            page_headers.append(str(tag.get_text(strip=True)))
+    else:
+        page_headers = None
+
+    if a_tags != []:
+        for tag in a_tags:
+            href = tag.get("href")
+
+            if href != None:
+                href = str(href).strip()
+            else:
+                href = None
+
+            page_links.append(
+                {
+                    "text": str(tag.get_text(strip=True)), 
+                    "link": href
+                })
+    else:
+        page_links = None
+
     parsed_dict["title"] = page_title
     parsed_dict["description"] = page_description
+    parsed_dict["headers"] = page_headers
+    parsed_dict["links"] = page_links
 
     return parsed_dict
